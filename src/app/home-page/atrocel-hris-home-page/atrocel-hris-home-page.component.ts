71329c69d8537cbc514ed107831eb503dbb9c68c
dhilruk.shan <dhilruk.shan@cloudsolution-sa.com>
2021-05-27 17:28:05 +0530

    added user role changes

--- src/app/home-page/atrocel-hris-home-page/atrocel-hris-home-page.component.ts
+++ src/app/home-page/atrocel-hris-home-page/atrocel-hris-home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GridOptions } from 'ag-grid-community';
+import { AltrocelServices } from '../../constant/altrocel-hris-services.service';
 
 @Component({
   selector: 'app-atrocel-hris-home-page',
@@ -12,10 +13,29 @@ export class AbcLabsHomePageComponent implements OnInit {
   opened: boolean = false;
   tabName: string = "WELCOME"
   userType: string;
-  constructor() { }
+  userName: string;
+  userData: any;
+
+  constructor(
+    private altrocelServices: AltrocelServices
+    ) { }
 
   ngOnInit() {
-    this.userType = localStorage.getItem('loggedUserType');
+    
+    this.userName = localStorage.getItem('loggedUserName');
+    this.getEmployeeDetailsByUsername(this.userName);
+  }
+
+  getEmployeeDetailsByUsername(username: string){
+    this.altrocelServices.getEmployeeByUsername(username).subscribe(res => {
+      if(res){
+        localStorage.setItem("loggedUserData", JSON.stringify(res));
+        this.userData = Object.assign(res);
+        if(this.userData){
+          this.userType = this.userData.jobTitle;
+        }
+      }
+    })
   }
 
   toggleSidebar() {
